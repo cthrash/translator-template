9e69248c02100beb1ef661908d5ad1005c569747
Chris Thrasher <cthrash@github.com>
2021-06-06 13:30:21 -0700

    Update output format

--- translator_template/app.py
+++ translator_template/app.py
@@ -1,7 +1,7 @@
 import argparse
 
 from docx import Document
-from docx.shared import Inches
+from docx.shared import Inches, Pt
 from docx.enum.style import WD_STYLE_TYPE
 
 from .parse import read
@@ -10,16 +10,25 @@ from .parse import read
 def generate(in_file: str, out_file: str) -> None:
     """Read the input specification from the in_file file, and generate a Word document at out_file."""
     document = Document()
-    page_style = document.styles.add_style("PagePara", WD_STYLE_TYPE.PARAGRAPH)  # noqa(F841)
-    panel_style = document.styles.add_style("PanelPara", WD_STYLE_TYPE.PARAGRAPH)
-    panel_style.paragraph_format.left_indent = Inches(0.5)
-    quote_style = document.styles.add_style("QuotePara", WD_STYLE_TYPE.PARAGRAPH)
-    quote_style.paragraph_format.left_indent = Inches(1.0)
+    normal_style = document.styles["Normal"]
+    normal_style.font.name = "Times New Roman"
+    normal_style.font.size = Pt(12)
+
+    def add_style(name: str, indent):
+        style = document.styles.add_style(name, WD_STYLE_TYPE.PARAGRAPH)
+        style.base_style = normal_style
+        style.paragraph_format.left_indent = Inches(indent)
+        return style
+
+    page_style = add_style("PagePara", 0)  # noqa(F841)
+    panel_style = add_style("PanelPara", 0.5)
+    quote_style = add_style("QuotePara", 1.0)
+
     with open(in_file, "rt") as f:
         for page, panels in read(f):
-            document.add_paragraph(f"Page {page}", style="PagePara")
+            document.add_paragraph(f"PAGE {page}", style="PagePara")
             for panel in range(panels):
-                document.add_paragraph(f"Panel {panel+1}", style="PanelPara")
+                document.add_paragraph(f"PANEL {panel+1}", style="PanelPara")
                 document.add_paragraph("", style="QuotePara")
     document.save(out_file)
 
--- translator_template/tests/test_app.py
+++ translator_template/tests/test_app.py
@@ -5,8 +5,9 @@ from translator_template.app import generate
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def test_generate():
     with TemporaryDirectory() as dir:
-        in_file = os.path.join(TEST_DIR, 'data', 'test.txt')
-        out_file = os.path.join(dir, 'test.docx')
+        in_file = os.path.join(TEST_DIR, "data", "test.txt")
+        out_file = os.path.join(dir, "test.docx")
         generate(in_file, out_file)
